c8fafb959616e9f524e7b67d960c1683e6b0bd00
xyxabcdiy <xyxabcdiy@gmail.com>
2015-05-18 21:46:53 +0800

    Splitting Across Files

---
+++ SplittingAcrossFiles/LettersOnlyValidator.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by woaitzy on 2015/5/18 0018.
+ */
+/// <reference path="Validation.ts" />
+module Validation {
+    var lettersRegexp = /^[A-Za-z]+$/;
+    export class LettersOnlyValidator implements StringValidator {
+        isAcceptable(s: string) {
+            return lettersRegexp.test(s);
+        }
+    }
+}
