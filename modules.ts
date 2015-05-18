d33c1215db77608ae1fb22f6900f379f1429ccf3
xyxabcdiy <xyxabcdiy@gmail.com>
2015-05-18 20:47:19 +0800

    create an interface named StringValidator

--- modules.ts
+++ modules.ts
@@ -4,3 +4,11 @@
 interface StringValidator {
     isAcceptable(s:string):boolean;
 }
+var letterRegexp = /^[A-Za-z]+$/;
+var numberRegexp = /^[0-9]+$/;
+
+class lettersOnlyValidator implements StringValidator{
+    isAcceptable(s:string){
+        return letterRegexp.test(s);
+    }
+}
