55601b6f11f8787e711b1116f00caa1e92033541
xyxabcdiy <xyxabcdiy@gmail.com>
2015-05-18 20:56:39 +0800

    finish code but not run

--- modules.ts
+++ modules.ts
@@ -7,8 +7,24 @@ interface StringValidator {
 var letterRegexp = /^[A-Za-z]+$/;
 var numberRegexp = /^[0-9]+$/;
 
-class lettersOnlyValidator implements StringValidator{
+class LettersOnlyValidator implements StringValidator{
     isAcceptable(s:string){
         return letterRegexp.test(s);
     }
 }
+class  ZipCodeValidator implements StringValidator{
+    isAcceptable(s:string){
+        return s.length === 5 && numberRegexp.test(s);
+    }
+}
+
+var strings = ['Hello', '98052', '101'];
+var validators:{[s:string]:StringValidator;} = {};
+validators['ZIP code'] = new ZipCodeValidator();
+validators['Letters only'] = new LettersOnlyValidator();
+
+strings.forEach(s=> {
+    for (var name in validators) {
+        console.log('"' + s + '" ' + (validators[name].isAcceptable(s) ? 'matches' : 'does not match') + name);
+    }
+});
